226602852f9f8bb91b27593d7388231f6dd58240
Wuantzin <danielsevla8@gmail.com>
2025-05-30 00:11:22 -0300

    Adição do arquivo .env para informações sensíveis

--- src/config/configdb.js
+++ src/config/configdb.js
@@ -1,11 +1,13 @@
 // Conexão com o MySQL
 const mysql = require('mysql2/promise');
+require('dotenv').config();
+
 
 const pool = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    password: '1234',
-    database: 'projeto_main',
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0
